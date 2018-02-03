62efcd565e7f757ecc5634a1fd83b39bf0262c47
vcani003 <vcani003@fiu.edu>
2018-02-03 13:17:09 -0500

    Merging back files

---
+++ app/src/main/java/com/easysailapp/easysail/Food.java
@@ -0,0 +1,51 @@
+package com.easysailapp.easysail;
+
+/**
+ * Created by veron on 2/3/2018.
+ */
+
+public class Food {
+    private String foodType;
+    private String foodName;
+    private String description;
+    private String price;
+
+    public Food(String foodType, String foodName, String description, String price) {
+        this.foodType = foodType;
+        this.foodName = foodName;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getFoodType() {
+        return foodType;
+    }
+
+    public void setFoodType(String foodType) {
+        this.foodType = foodType;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
--- app/src/main/java/com/easysailapp/easysail/FoodActivity.java
+++ app/src/main/java/com/easysailapp/easysail/FoodActivity.java
@@ -1,13 +1,81 @@
 package com.easysailapp.easysail;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class FoodActivity extends AppCompatActivity {
+    private RecyclerView mFoodRecyclerView;
+    private FoodAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
+
+        mFoodRecyclerView = (RecyclerView) findViewById(R.id.food_recycler_layout);
+        mFoodRecyclerView.setLayoutManager(new LinearLayoutManager(FoodActivity.this));
+
+        FoodMenu mFoodMenu = new FoodMenu();
+
+        mAdapter = new FoodAdapter(mFoodMenu.getMenuItems());
+        mFoodRecyclerView.setAdapter(mAdapter);
+
+
     }
+
+    private class FoodHolder extends RecyclerView.ViewHolder{
+
+        private TextView mFoodName;
+        private TextView mDescription;
+        private TextView mPrice;
+        private Food mFood;
+
+        public FoodHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.food_item, parent, false));
+            mFoodName = (TextView) itemView.findViewById(R.id.food_name_text);
+            mDescription = (TextView) itemView.findViewById(R.id.description_text);
+            mPrice = (TextView) itemView.findViewById(R.id.price_text);
+        }
+        public void bind(Food food)
+        {
+            mFood = food;
+            mFoodName.setText(mFood.getFoodName());
+            mDescription.setText(mFood.getDescription());
+            mPrice.setText(mFood.getPrice());
+        }
+    }
+
+    private class FoodAdapter extends RecyclerView.Adapter<FoodHolder>{
+        private List<Food> mFoods;
+
+        public FoodAdapter(List<Food> foods){
+            mFoods = foods;
+        }
+
+        @Override
+        public FoodHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(FoodActivity.this);
+            return new FoodHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(FoodHolder holder, int position) {
+            Food food = mFoods.get(position);
+            holder.bind(food);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mFoods.size();
+        }
+    }
+
 }
