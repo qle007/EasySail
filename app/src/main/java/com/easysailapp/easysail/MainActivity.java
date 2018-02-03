ab4c245cebebc060b6f055a088b17522dbd48177
qle007 <qle007@fiu.edu>
2018-02-03 02:45:06 -0500

    added sum stuff

--- app/src/main/java/com/easysailapp/easysail/MainActivity.java
+++ app/src/main/java/com/easysailapp/easysail/MainActivity.java
@@ -1,13 +1,47 @@
 package com.easysailapp.easysail;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText iDEditText;
+    private Button signInButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iDEditText = (EditText) findViewById(R.id.card_id_edit_text);
+        signInButton = (Button) findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(iDEditText.getText().toString().length() > 0 && iDEditText.getText().toString().equals("123456"))
+                {
+                    Intent intent = new Intent(MainActivity.this, SplashScreen.class);
+                    startActivity(intent);
+                }
+                else
+                {
+                    AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+                    builder1.setMessage("ID does not exist!");
+                    builder1.setNegativeButton(
+                            "OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+                }
+            }
+        });
     }
 }
